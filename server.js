8a6d49610c6f0bf8e0c50623d0819761957b6b24
jessie-computes <60405600+jessie-computes@users.noreply.github.com>
2020-04-03 23:35:10 -0400

    cannot get the saving and returning of saved notes to work

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var jsonFile = require("./db/db.json");
 
 var app = express();
 
@@ -13,18 +14,26 @@ app.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.get("/api/notes", function(req, res){
-    fs.readFile("./db/db.json", "utf8", function(err, data){
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) throw err;
+        var jsonData = data;
         console.log(data);
-        return data;
-    })
+    });
+    return res.json(jsonData);
 });
 
+app.post("/api/notes", function(req, res){
+    fs.appendFile("./db/db.json", req.body, (err) => {
+        if (err) throw err;
+        console.log(req.body)
+    })
+    return req.body
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
